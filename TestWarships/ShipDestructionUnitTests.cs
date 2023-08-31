83b2b0cc995f32148779b652d112d32ecb172119
zamorski-woj <zamorski.woj@gmail.com>
2023-08-31 18:58:26 +0200

    add unit tests for destroying ships

---
+++ TestWarships/ShipDestructionUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warships;
+using static Warships.WarshipsGame;
+
+
+namespace TestWarships
+{
+    public class ShipDestructionUnitTests
+    {
+
+        [Theory]
+        [InlineData(0, 0, Direction.South, 1)]
+        [InlineData(0, 4, Direction.East, 3)]
+        [InlineData(3, 3, Direction.North, 2)]
+        [InlineData(2, 3, Direction.West, 2)]
+        public void Destroy_ShouldSinkCells(int x, int y, Direction direction, int length)
+        {
+            Tuple<CellType[,], CellType[,]> bothMaps = CreateMaps(5, CellType.Water);
+            Ship ship = new(length, direction, new Tuple<int, int>(x, y));
+            PlaceShip(bothMaps.Item1, ship);
+            PlaceShip(bothMaps.Item1, ship);
+            ship.Destroy(bothMaps.Item1);
+            List < Tuple<int, int> > coordinates= ship.GetCoordinates();
+            foreach(Tuple<int,int> c in coordinates)
+            {
+                bothMaps.Item1[c.Item1, c.Item2].Should().Be(CellType.Sunken);
+            }
+        }
+
+        [Theory]
+        [InlineData(0, 0, Direction.South, 1)]
+        [InlineData(0, 4, Direction.East, 3)]
+        [InlineData(3, 3, Direction.North, 2)]
+        [InlineData(2, 3, Direction.West, 2)]
+        public void ShootingShip_ShouldDestroyIt(int x, int y, Direction direction, int length)
+        {
+            Tuple<CellType[,], CellType[,]> bothMaps = CreateMaps(5, CellType.Water);
+            Ship ship = new(length, direction, new Tuple<int, int>(x, y));
+            PlaceShip(bothMaps.Item1, ship);
+            PlaceShip(bothMaps.Item1, ship);
+
+            List<Tuple<int, int>> coordinates = ship.GetCoordinates();
+            foreach (Tuple<int, int> c in coordinates)
+            {
+                Shoot(bothMaps.Item1, c).Should().NotBe(CellType.Water);
+                Shoot(bothMaps.Item1, c).Should().NotBe(CellType.Unknown);
+            }
+            foreach (Tuple<int, int> c in coordinates)
+            {
+                bothMaps.Item1[c.Item1, c.Item2].Should().Be(CellType.Sunken);
+            }
+            ship.sunken.Should().BeTrue();
+        }
+
+     
+    }
+}
