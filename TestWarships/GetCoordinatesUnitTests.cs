4af19343cc1e8ea5c097ba77695f0a1007c4e52e
zamorski-woj <zamorski.woj@gmail.com>
2023-08-31 13:34:22 +0200

    add unit tests for getting ship coordinates

---
+++ TestWarships/GetCoordinatesUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Warships;
+using static Warships.WarshipsGame;
+
+namespace TestWarships
+{
+    public class GetCoordinatesUnitTests
+    {
+        [Theory]
+        [InlineData(0, 0, Direction.South, 1)]
+        [InlineData(0, 4, Direction.East, 3)]
+        [InlineData(3, 3, Direction.North, 2)]
+        [InlineData(2, 3, Direction.West, 2)]
+        public void GetCoordinates(int x, int y, Direction direction, int length)
+        {
+            //Tuple<CellType[,], CellType[,]> bothMaps = CreateMaps(5, CellType.Water);
+            Ship ship = new(length, direction, new Tuple<int, int>(x, y));
+            List<Tuple<int, int>> coordinates =  ship.GetCoordinates();
+            coordinates.Count.Should().Be(length);
+        }
+
+    }
+}
