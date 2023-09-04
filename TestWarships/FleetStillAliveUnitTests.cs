c730fa43488d549e8b887a8842460e19a456fca7
zamorski-woj <zamorski.woj@gmail.com>
2023-09-04 10:41:25 +0200

    add fleet unit tests

---
+++ TestWarships/FleetStillAliveUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using Warships;
+using static Warships.WarshipsGame;
+
+
+namespace TestWarships
+{
+    public class FleetStillAliveUnitTests
+    {
+
+        
+            [Fact]
+            public void FleetStillAlive_ShouldReturnFalseWhenEmpty()
+            {
+            var players = CreateTwoPlayers();
+            players.Item1.FleetStillAlive().Should().BeFalse();
+            players.Item2.FleetStillAlive().Should().BeFalse();
+             }
+
+        [Theory]
+        [InlineData(0, 0, Direction.South, 1)]
+        [InlineData(0, 4, Direction.East, 3)]
+        [InlineData(3, 3, Direction.North, 2)]
+        [InlineData(2, 3, Direction.West, 2)]
+        public void FleetStillAlive_ShouldReturnTrueWhenAtLeastOneShip(int x, int y, Direction direction, int length)
+        {
+            var players = CreateTwoPlayers();
+            Map map = players.Item1.map;
+            Ship ship = new(length, direction, new Tuple<int, int>(x, y));
+            map.PlaceShip(ship);
+            players.Item1.FleetStillAlive().Should().BeTrue();
+            players.Item2.FleetStillAlive().Should().BeFalse();
+            players.Item2.map.PlaceShip(ship);
+            players.Item2.FleetStillAlive().Should().BeTrue();
+
+        }
+    }
+
+}
+
---
+++ TestWarships/GenerateFleetUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Warships;
+using static Warships.WarshipsGame;
+
+namespace TestWarships
+{
+    public class GenerateFleetUnitTests
+    {
+
+    
+        [Fact]
+    public void GenerateFleet_ShouldGiveAsManyShipsAsRequested()
+    {
+        var players = CreateTwoPlayers();
+        List<int> list = new() { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2, 1, 1 };
+        players.Item1.GenerateFleet(list);
+        players.Item2.GenerateFleet(list);
+
+        players.Item1.fleet.Count.Should().Be(12);
+        players.Item2.fleet.Count.Should().Be(12);
+    }
+}
+}
